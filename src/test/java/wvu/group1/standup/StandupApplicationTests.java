2849fae6fccfef7dfdc1c2076c7a1389c96f21e5
cwm <coleman.mchose@agile5technologies.com>
2019-01-22 22:02:13 -0500

    initialized project

---
+++ src/test/java/wvu/group1/standup/StandupApplicationTests.java
@@ -0,0 +1,17 @@
+package wvu.group1.standup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StandupApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+

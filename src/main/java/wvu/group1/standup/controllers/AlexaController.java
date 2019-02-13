da567fbf6b326a7b0046a0d8d711be37730b64aa
cwm <coleman.mchose@agile5technologies.com>
2019-02-13 17:15:16 -0500

    some skeleton code

---
+++ src/main/java/wvu/group1/standup/controllers/AlexaController.java
@@ -0,0 +1,17 @@
+public class AlexaController{
+/*
+	
+	@Autowired
+	private StandupApiService standupApiService;
+	
+	public void recieveVoiceStandup(Standup s){
+		standupApiService.saveStandup(s.handleTranscription());
+	}
+
+	public Standup retrieveStandup(){
+		return null;
+	}
+
+
+*/
+}
---
+++ src/main/java/wvu/group1/standup/controllers/WebController.java
@@ -0,0 +1,28 @@
+//@Controller
+public class WebController{
+/*
+ 	
+	@Autowired
+	private StandupApiService standupApiService;
+
+	
+	public String recieveTextStandup(Standup s, Principal p){
+		standupApiService.saveStandup(s);
+		return "home";
+	} 		
+	
+	public Standup retrieveStandup(SearchCriteria sc){
+		return standupApiService.getStandup(sc);
+	}
+
+	public List<Standup> retrieveUserStandups(SearchCriteria sc){
+		return standupApiService.getStandups(sc);
+	}
+
+	public List<Standup> retrieveGroupStandups(SearchCriteria sc){
+		return standupApiService.getStandups(sc);
+	}
+	
+
+*/
+}
